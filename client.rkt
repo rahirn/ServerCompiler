28f5f3bb3e375c23ec786f28da00fd6555c9555d
Josh J <jocjohns@mtu.edu>
2015-12-13 13:39:53 -0500

    Changed layout.

---
+++ client.rkt
@@ -0,0 +1,5 @@
+#lang racket
+
+(define (connect-server port)
+  ;; code
+  )
--- server.rkt
+++ server.rkt
@@ -2,3 +2,9 @@
 (define (launch-server port)
   ;; Your code here
   )
+
+(define (compile-file source object)
+  (with-output-to-file object	
+    (lambda () (compile (open-input-file source)))    
+    #:exists 'replace)
+  )
